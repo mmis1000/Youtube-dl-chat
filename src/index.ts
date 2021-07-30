58cc6098e867a4b948916e3ca3c8b598cd7e58ab
Mmis1000 <mmis1000@yahoo.com.tw>
2021-07-30 15:45:19 +0800

    Fix cjs exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+export * from './comment-client'
+export * from './fetch-with-cookie'
+export * from './assets-downloader'
+export * from './interfaces'
+export * from './interfaces-youtube-response'
+export * from './parser'
